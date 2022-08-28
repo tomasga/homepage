64e46bed20cdee44917baee54ecad96f681febf3
Tomasz Gawlowski <podajemejla@gmail.com>
2022-08-28 17:09:59 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Cześć! Cieszę się że tu zajrzałeś/łaś. To moje pierwsze kroki, ale myślę, że niebawem będzie lepiej:)");
+
+let chLangBtnPl = document.querySelector(".header__button--pl");
+let chLangBtnEn = document.querySelector(".header__button--en");
+let langPlEn = document.querySelectorAll(".langPlEn");
+let age = document.querySelector(".age");
+
+chLangBtnPl.addEventListener('click', () => {
+    langPlEn.forEach(p => p.classList.toggle('hide'));
+});
+
+chLangBtnEn.addEventListener('click', () => {
+    langPlEn.forEach(p => p.classList.toggle('hide'));
+});
+
+
+
+let birthday = new Date(new Date().getFullYear(), 8, 10);
+let years = (+birthday > +new Date() ? birthday.getFullYear() - 1974 - 1 : birthday.getFullYear() - 1974);
+age.innerText = years;
