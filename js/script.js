8147d78e60f6a3b2c8e5c51320c93a62c3308400
Tomasz <podajemejla@gmail.com>
2022-12-26 23:11:41 +0100

    Functions added to js.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
-console.log("Cześć! Cieszę się że tu zajrzałeś/łaś. To moje pierwsze kroki, ale myślę, że niebawem będzie lepiej:)");
+const welcome = () => {
+    console.log("Cześć! Cieszę się że tu zajrzałeś/łaś. To moje pierwsze kroki, ale myślę, że niebawem będzie lepiej:)");
+}
 
-let chLangBtnPl = document.querySelector(".header__button--pl");
-let chLangBtnEn = document.querySelector(".header__button--en");
-let langPlEn = document.querySelectorAll(".js-langPlEn");
-let age = document.querySelector(".age");
-let ageEn = document.querySelector(".ageEn");
+const ageCalculation = () => {
+    const age = document.querySelector(".age");
+    const birthday = new Date(new Date().getFullYear(), 8, 10);
+    const years = (+birthday > +new Date() ? birthday.getFullYear() - 1974 - 1 : birthday.getFullYear() - 1974);
+    age.innerText = years;
+}
 
-chLangBtnPl.addEventListener('click', () => {
-    langPlEn.forEach(p => p.classList.toggle('hide'));
-});
+const sectionsHideToggle = () => {
+    const chLangBtnPl = document.querySelector(".header__button--pl");
+    const chLangBtnEn = document.querySelector(".header__button--en");
+    const langPlEn = document.querySelectorAll(".js-langPlEn");
 
-chLangBtnEn.addEventListener('click', () => {
-    langPlEn.forEach(p => p.classList.toggle('hide'));
-});
+    chLangBtnPl.addEventListener('click', () => {
+        langPlEn.forEach(p => p.classList.toggle('hide'));
+    });
 
+    chLangBtnEn.addEventListener('click', () => {
+        langPlEn.forEach(p => p.classList.toggle('hide'));
+    });
+}
 
+const init = () => {
+welcome();
+ageCalculation();
+sectionsHideToggle();
+}
 
-let birthday = new Date(new Date().getFullYear(), 8, 10);
-let years = (+birthday > +new Date() ? birthday.getFullYear() - 1974 - 1 : birthday.getFullYear() - 1974);
-age.innerText = years;
-ageEn.innerText = years;
+init();
